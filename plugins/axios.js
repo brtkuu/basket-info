9c64376248a4c4dfeba263fa23b08414fa95e203
brtkuu <bartoszposert5@gmail.com>
2021-11-06 14:21:10 +0100

    api key

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 export default function ({ $axios, app, store }) {
   $axios.onRequest((config) => {
-    config.headers.common['x-rapidapi-key'] = process.env.API_KEY;
+    config.headers.common['x-rapidapi-key'] = '41d322edb2msh41c388eb386d403p1fd5dfjsn44ba1965554a';
   })
 }
