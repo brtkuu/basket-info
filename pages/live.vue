a2a3f55c23879bde0749937ccc8e223f436f0f9a
Przemek Michalski <michalskiprzemyslaw98@gmail.com>
2022-01-10 19:33:25 +0100

    Live games test

--- pages/live.vue
+++ pages/live.vue
@@ -1,10 +1,38 @@
 <template>
   <!--  <template v-if="{{result}} != 0"> -->
   <div class="live__container">
-    <div class="live__liveContainer">
-      <p v-if="!games.length">
-        There are no games avaliable live at the moment
-      </p>
+    <div
+        class="live__body"
+        v-if="!liveGames.length"
+    >
+      <h1 v-if="!liveGames.length">
+        There are currently no live games but below are games from the last 24 hours
+      </h1>
+      <div
+          v-for="match in lastGames"
+          :key="match.id"
+          class="live__single-match"
+      >
+        <p>
+          {{match.hTeam.shortName}} {{match.hTeam.points}} : {{match.vTeam.points}} {{match.vTeam.shortName}}
+        </p>
+        <div>
+          <nuxt-link :to="`/team/${match.hTeam.id}`">
+            <img
+                class="live__team-logo"
+                :src="match.hTeam.logo"
+            >
+          </nuxt-link>
+        </div>
+        <div>
+          <nuxt-link :to="`/team/${match.vTeam.id}`">
+            <img
+                class="live__team-logo"
+                :src="match.vTeam.logo"
+            >
+          </nuxt-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,25 +40,48 @@
 <script>
 class Match {
   constructor (data) {
-    this.gameId = data?.gameId || '';
+    this.id = data?.gameId || '';
+    this.hTeam = new Team(data?.hTeam);
+    this.vTeam = new Team(data?.vTeam);
+  }
+}
+
+class Team {
+  constructor (data) {
+    this.id = data?.teamId || '';
+    this.shortName = data?.shortName || '';
+    this.points = data?.score.points || '';
+    this.logo = data?.logo || '';
   }
 }
 export default {
   name: 'Live',
   data () {
     return {
-      games: []
+      lastGames: [],
+      liveGames: [],
+      today: new Date()
     };
   },
   async mounted () {
     await this.getLiveMatch();
+    await this.getLastThreeMatches();
   },
   methods: {
     async getLiveMatch () {
       try {
         const res = await this.$axios.get('/games/live/');
-        this.games = res.data.api.games.map(game => new Match(game));
-        console.log(this.games);
+        this.liveGames = res.data.api.games.map(game => new Match(game));
+        console.log(this.liveGames);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getLastThreeMatches () {
+      try {
+        const res = await this.$axios.get(`https://api-nba-v1.p.rapidapi.com/games/date/${this.today.toISOString().split('T')[0]}`);
+        this.lastGames = res.data.api.games.map(match => new Match(match));
+        console.log(this.lastGames);
       } catch (e) {
         console.log(e);
       }
